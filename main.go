f2a657e95705fb9b18fc732ecb115666b5f00403
Benjamin Froelich <bbenif@gmail.com>
2024-04-20 19:13:51 +0200

    game state data structure

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type GameState struct {
+	Board [4][4]int
+	Step  int
+}
+
+func MakeGame() GameState {
+	return GameState{} // just empty for now
+}
+
 func main() {
+	game := MakeGame()
 	fmt.Println("Hello Martin!")
+	fmt.Println(game)
 }
