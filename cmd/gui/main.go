217846d954ee2fab102c8a1e6de7c186b6d5e318
Benjamin Froelich <bbenif@gmail.com>
2024-05-02 17:50:33 +0200

    gui stuff

---
+++ cmd/gui/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	for !window.ShouldClose() {
+
+		gl.ClearColor(1, 1, 0, 0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		// Render game
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
